92970c11c261ea0446875f06dd08bdc3851edfc3
Abhineeth Adiraju <abhineeth00@gmail.com>
2023-09-30 15:48:27 -0700

    * Added Placeholder Pages * Added Landing Component * Moved initial home page to /home * configured prettier and postcss

---
+++ src/components/sections/landingSection.tsx
@@ -0,0 +1,13 @@
+export default function Landing() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col justify-center text-8xl p-3 font-semibold">
+        <div className="gap-3 items-baseline flex-shrink-0 sm:text-center">Abhineeth</div>
+        <div className="gap-3 items-baseline flex-shrink-0 sm:text-center">Adiraju</div>
+      </div>
+      <div className="flex flex-col justify-center p-3 text-4xl font-extralight">
+        Work in Progress....
+      </div>
+    </div>
+  );
+}
