2bf25ac1431b5ab2c7964aaee9ca8de13fa585fe
Abhineeth Adiraju <abhineeth00@gmail.com>
2023-12-24 16:40:48 -0800

    * Added pipeline svg

--- src/components/sections/aboutSection.tsx
+++ src/components/sections/aboutSection.tsx
@@ -1,9 +1,11 @@
+import PipelineAnimation from "../PipelineAnimation";
+
 const About = () => {
   return (
-    <div className="z-10 mt-5 flex h-[100vh] w-full flex-col">
-      <div className="flex flex-col gap-6 p-5 tracking-tight">
-        <div className="text-5xl font-semibold">About</div>
-        <div className="mt-7 flex w-[40%] flex-col gap-6 text-2xl font-extralight">
+    <div className="z-10 mt-5 flex h-[100vh] w-full flex-col p-5 tracking-tight">
+      <div className="text-6xl font-semibold mb-[10vh]">About</div>
+      <div className="flex h-full justify-center">
+        <div className="flex flex-[3] flex-col gap-6 text-3xl font-extralight">
           <p>
             I am a DevOps Engineer interested in building solutions driven by a
             purpose to <span className="font-semibold">make a difference</span>.
@@ -16,9 +18,15 @@ const About = () => {
           </p>
           <p>
             Having been immersed in the arts since my childhood, I enjoy coming
-            up with <span className="font-semibold">creative</span> and <span className="font-semibold">elegant</span>{" "}
-            solutions to problems in every aspect of my life.
+            up with <span className="font-semibold">creative</span> and{" "}
+            <span className="font-semibold">elegant</span> solutions to problems
+            in every aspect of my life.
           </p>
+        </div>
+        <div className="flex flex-[3] flex-col">
+          <div className="mx-[10vh]">
+            <PipelineAnimation />
+          </div>
         </div>
       </div>
     </div>
