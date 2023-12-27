7a717db9f084e01ed193e36f33af14e61a4952c4
Abhineeth Adiraju <abhineeth00@gmail.com>
2023-12-27 15:59:41 -0800

    springify animations on svg

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,13 @@
 import { MotionValue, useSpring } from "framer-motion";
 
+const springConfigs = {
+  stiffness: 100,
+  damping: 30,
+  restDelta: 0.001,
+};
 function useSpringify(rawMotionValue: MotionValue<number>) {
-    const springifiedValue = useSpring(rawMotionValue, {
-      stiffness: 100,
-      damping: 30,
-      restDelta: 0.001,
-    });
-    return springifiedValue;
-  }
+  const springifiedValue = useSpring(rawMotionValue, springConfigs);
+  return springifiedValue;
+}
 
-
-  export {
-    useSpringify
-  }
+export { useSpringify, springConfigs };
