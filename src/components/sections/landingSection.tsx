67ede651a5b599d57ffa62dab9800461246c4151
Abhineeth Adiraju <abhineeth00@gmail.com>
2024-10-20 17:21:34 -0700

    z-axis shenanigans

--- src/components/sections/landingSection.tsx
+++ src/components/sections/landingSection.tsx
@@ -40,8 +40,8 @@ const Landing = () => {
   ];
 
   return (
-    <div className="z-10 flex h-[100vh] w-full flex-col justify-center">
-      <div className="flex flex-col justify-center gap-2 p-5 text-6xl font-bold tracking-tighter drop-shadow-lg md:text-7xl md:drop-shadow-xl">
+    <div className="flex h-[100vh] w-full flex-col justify-center">
+      <div className="flex flex-col justify-center gap-2 p-5 text-6xl font-bold tracking-tighter drop-shadow-lg md:text-7xl md:drop-shadow-xl z-20">
         <div className="flex-shrink-0 items-baseline sm:text-center">
           Abhineeth
         </div>
@@ -49,14 +49,14 @@ const Landing = () => {
           Adiraju
         </div>
       </div>
-      <div className="flex w-full flex-row items-center justify-center gap-[2.5%] font-normal tracking-tighter md:gap-4 md:p-5 md:text-3xl">
+      <div className="flex w-full flex-row items-center justify-center gap-[2.5%] font-normal tracking-tighter md:gap-4 md:p-5 md:text-3xl z-20">
         <div className="">DevOps Engineer</div>
         <div>|</div>
         <div className="">Artist</div>
         <div>|</div>
         <div className="">Home &quot;Chef&quot;</div>
       </div>
-      <div className="absolute hidden h-[70vh] w-full lg:block">
+      <div className="absolute hidden h-[70vh] w-full z-10 lg:block">
         {windowMap.map((window, index) => {
           return (
             <div
